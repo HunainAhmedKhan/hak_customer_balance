3b7fa6e0642470df44692ccc93e373d2addc461b
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2021-04-26 15:42:29 +0500

    Update __manifest__.py

--- hak_customer_balance/__manifest__.py
+++ hak_customer_balance/__manifest__.py
@@ -10,7 +10,9 @@
     """,
 
     'author': "HAK solutions",
-    'website': "http://www.HAKsolutions.com",
+    'category': 'Base',
+    'website': 'https://www.HAKsolutions.com/',
+    'license': 'AGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
